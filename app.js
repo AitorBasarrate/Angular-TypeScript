3ea18be6261a24de82f9766cc62428b6d82eaa5e
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2020-06-28 19:51:19 +0200

    restart

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
 "use strict";
-let x;
-let i;
-const CONT = 10;
-let erantzuna;
-const FIBONACCI = () => {
-    x = [0, 1];
-    while (CONT < 10) {
-        if (x[i] > x[i - 1]) {
-            erantzuna = x[i] + x[i - 1];
-            x[i + 1] = erantzuna;
-        }
-        console.log(x[i]);
-        i++;
-    }
-};
-FIBONACCI;
