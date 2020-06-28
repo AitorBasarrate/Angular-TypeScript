77e03106e2145dd907d4466bb9c3ec6f5ff897f8
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2020-06-28 19:48:53 +0200

    funcion fibonacci

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 "use strict";
 let x;
 let i;
+const CONT = 10;
 let erantzuna;
 const FIBONACCI = () => {
-    if (x[i] > x[i - 1]) {
-        erantzuna = x[i] + x[i - 1];
-        x[i + 1] = erantzuna;
+    x = [0, 1];
+    while (CONT < 10) {
+        if (x[i] > x[i - 1]) {
+            erantzuna = x[i] + x[i - 1];
+            x[i + 1] = erantzuna;
+        }
+        console.log(x[i]);
+        i++;
     }
-    i++;
 };
+FIBONACCI;
--- app.ts
+++ app.ts
@@ -1,11 +1,19 @@
 let x:number[];
 let i:number;
+const CONT = 10
 let erantzuna:number;
 
 const FIBONACCI = () => {
-    if (x[i] > x[i-1]) {
-        erantzuna = x[i] + x[i-1];
-        x[i+1] = erantzuna
+    x = [0, 1];
+    while (CONT < 10) {
+        if (x[i] > x[i-1]) {
+            erantzuna = x[i] + x[i-1];
+            x[i+1] = erantzuna
+        }
+        console.log(x[i]);
+        i++;
     }
-    i++;
+    
 }
+
+FIBONACCI;
